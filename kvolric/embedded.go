8a8346df768552b4e369636d282f05a62d93b777
royalcat <k.adamovich20@gmail.com>
2024-05-17 00:35:39 +0300

    olric

---
+++ kvolric/embedded.go
@@ -0,0 +1,131 @@
+package kvolric
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/buraksezer/olric"
+	"github.com/royalcat/kv"
+)
+
+func NewEmbedded[V any](db *olric.Olric, bucket string) (kv.Store[string, V], error) {
+	e := db.NewEmbeddedClient()
+	dm, err := e.NewDMap(bucket)
+	if err != nil {
+		log.Fatalf("olric.NewDMap returned an error: %v", err)
+	}
+
+	return &embedded[V]{
+		c:  e,
+		dm: dm,
+	}, nil
+
+}
+
+type Options struct {
+	kv.Options
+}
+
+type embedded[V any] struct {
+	Options
+	c  *olric.EmbeddedClient
+	dm olric.DMap
+}
+
+var _ kv.Store[string, struct{}] = (*embedded[struct{}])(nil)
+
+// Delete implements kv.Store.
+func (s *embedded[V]) Delete(ctx context.Context, k string) error {
+	_, err := s.dm.Delete(ctx, k)
+	return err
+}
+
+// Get implements kv.Store.
+func (s *embedded[V]) Get(ctx context.Context, k string) (v V, found bool, err error) {
+	resp, err := s.dm.Get(ctx, k)
+	if err != nil {
+		if errors.Is(err, olric.ErrKeyNotFound) {
+			return v, false, nil
+		}
+
+		return v, found, err
+	}
+
+	data, err := resp.Byte()
+	if err != nil {
+		return v, found, err
+	}
+
+	err = s.Codec.Unmarshal(data, &v)
+	return v, true, err
+}
+
+// Range implements kv.Store.
+func (s *embedded[V]) Range(ctx context.Context, iter kv.Iter[string, V]) error {
+	it, err := s.dm.Scan(ctx)
+	if err != nil {
+		return err
+	}
+	defer it.Close()
+	for it.Next() {
+		k := it.Key()
+
+		v, found, err := s.Get(ctx, k)
+		if err != nil {
+			return err
+		}
+		if !found {
+			continue
+		}
+
+		if !iter(k, v) {
+			break
+		}
+
+	}
+
+	return nil
+}
+
+// RangeWithPrefix implements kv.Store.
+func (s *embedded[V]) RangeWithPrefix(ctx context.Context, k string, iter kv.Iter[string, V]) error {
+	it, err := s.dm.Scan(ctx, olric.Match("^"+k))
+	if err != nil {
+		return err
+	}
+	defer it.Close()
+	for it.Next() {
+		k := it.Key()
+
+		v, found, err := s.Get(ctx, k)
+		if err != nil {
+			return err
+		}
+		if !found {
+			continue
+		}
+
+		if !iter(k, v) {
+			break
+		}
+
+	}
+
+	return nil
+}
+
+// Set implements kv.Store.
+func (s *embedded[V]) Set(ctx context.Context, k string, v V) error {
+	data, err := s.Codec.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	return s.dm.Put(ctx, k, data)
+}
+
+// Close implements kv.Store.
+func (s *embedded[V]) Close(ctx context.Context) error {
+	return s.c.Close(ctx)
+}
---
+++ kvolric/go.mod
@@ -0,0 +1,43 @@
+module github.com/royalcat/kv/kvolric
+
+go 1.22.3
+
+require (
+	github.com/buraksezer/olric v0.5.6
+	github.com/royalcat/kv v0.0.0-20240516210312-aa6aa35b7160
+)
+
+require (
+	github.com/RoaringBitmap/roaring v1.2.1 // indirect
+	github.com/armon/go-metrics v0.0.0-20180917152333-f0300d1749da // indirect
+	github.com/bits-and-blooms/bitset v1.2.0 // indirect
+	github.com/buraksezer/consistent v0.10.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-msgpack v0.5.3 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/hashicorp/logutils v1.0.0 // indirect
+	github.com/hashicorp/memberlist v0.5.0 // indirect
+	github.com/miekg/dns v1.1.45 // indirect
+	github.com/mschoch/smat v0.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
+	github.com/tidwall/btree v1.1.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/redcon v1.6.2 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/tools v0.1.6-0.20210726203631-07bc1bf47fb2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
